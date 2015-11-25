6d063869ed1774fa14815ba26f30080b38b2c40a
Dane Springmeyer <dane@mapbox.com>
2015-11-25 08:47:24 -0500

    more debugging of win syntax

--- scripts/build-appveyor.bat
+++ scripts/build-appveyor.bat
@@ -49,10 +49,11 @@ IF EXIST "%ProgramFiles%\nodejs" ^
     ECHO copying local node.exe to "%ProgramFiles%\nodejs\node.exe" && COPY /Y node.exe "%ProgramFiles%\nodejs\"
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
-ECHO preparing to check shared libs
+ECHO preparing to check shared libs in "%ProgramFiles%\nodejs"
 IF EXIST "%ProgramFiles%\nodejs" ^
-    ECHO checking shared libs for new node.exe && python test\check_shared_libs.py %ProgramFiles%\nodejs\
+    ECHO checking shared libs for new node.exe && python test\check_shared_libs.py "%ProgramFiles%\nodejs"
 
+ECHO preparing to delete existing node.exe at "%ProgramFiles(x86)%\nodejs"
 IF DEFINED ProgramFiles(x86) ^
     IF EXIST "%ProgramFiles(x86)%\nodejs" ^
         IF EXIST "%ProgramFiles(x86)%\nodejs\node.exe" ^
@@ -60,14 +61,16 @@ IF DEFINED ProgramFiles(x86) ^
             DEL /F "%ProgramFiles(x86)%\nodejs\node.exe"
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
+ECHO preparing to copy into place new node.exe
 IF DEFINED ProgramFiles(x86) ^
     IF EXIST "%ProgramFiles(x86)%\nodejs" ^
         ECHO copying to "%ProgramFiles(x86)%\nodejs\node.exe" && COPY /Y node.exe "%ProgramFiles(x86)%\nodejs\"
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
+ECHO preparing to check shared libs in "%ProgramFiles(x86)%\nodejs"
 IF DEFINED ProgramFiles(x86) ^
     IF EXIST "%ProgramFiles(x86)%\nodejs" ^
-        ECHO checking shared libs for new node.exe && python test\check_shared_libs.py %ProgramFiles(x86)%\nodejs\
+        ECHO checking shared libs for new node.exe && python test\check_shared_libs.py "%ProgramFiles(x86)%\nodejs"
 
 ::delete node.exe in current directory, that newer npm versions put stuff into the right directories
 IF EXIST node.exe DEL node.exe
